4bf5e3fb2af4a82e22a62be0b7d1b6277e2a39a3
Jagat Jung Lakandri B.K <star5lakandri@gmail.com>
2021-08-28 21:56:08 +0545

    Making registeration happen

--- database/userdata.php
+++ database/userdata.php
@@ -1,58 +1,55 @@
-<?php 
+<?php
+session_start(); 
 include "connection.php";
-function tokenGen($num){
-    $charset = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    $token="";
-    for($i=0;$i<$num;$i++){
-        $token=$token."".$charset[rand(0,61)];
-    }
-    return $token;
-}
-if(isset($_POST['dob'])&&isset($_POST['hby'])&&isset($_POST['ohby'])&&isset($_POST['othby'])&&isset($_POST['jt'])&&isset($_POST['hby'])){
-    if ($conn->connect_error) {
+
+if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
       }
     else{
-        $pname=$_SESSION['pname'];
-        $token=tokenGen(20);
-        $sql = "INSERT INTO profile (
-        token, 
-        bio,	
-        dob,	
-        fb,	
-        ln,	
-        wa,	
-        pimg,	
-        hone,	
-        htwo,	
-        hthree,	
-        job,	
-        org,
-        location,	
-        position,	
-        Gender)
+        $token=$_SESSION['token']; 
+        $bio="random honi";	
+      
+        $dob="random honi";	
+        $fb="random honi";		
+        $ln="random honi";	
+        $wa="random honi";	
+        $pimg="random honi";	
+        $hone="random honi";	
+        $htwo="random honi";	
+        $job="random honi";
+        $hthree="Random";	
+        $org="random honi";
+        $location="random honi";	
+        $position="random honi";	
+        $Gender="female";
+
+        $sql = "INSERT INTO `profile` (`token`, `bio`, `dob`, `fb`, `ln`, `wa`, `pimg`, `hone`, `htwo`, `hthree`, `job`, `org`, `location`, `position`, `Gender`) 
         VALUES (
-        token, 
-        bio,	
-        dob,	
-        fb,	
-        ln,	
-        wa,	
-        pimg,	
-        hone,	
-        htwo,	
-        hthree,	
-        job,	
-        org,
-        location,	
-        position,	
-        Gender)";
+        '$token', 
+        '$bio',	
+        '$dob',	
+        '$fb',	
+        '$ln',	
+        '$wa',	
+        '$pimg',	
+        '$hone',	
+        '$htwo',
+        '$hthree',	
+        '$job',	
+        '$org',
+        '$location',	
+        '$position',	
+        '$Gender'
+        )";
+
         $result = $conn->query($sql);
+
         if($result){
-            header("Location:login.php");
-        }else{
-            echo "somethinf went wrong";
+            header("Location:../html/login.php");
+        }
+        else{
+            echo mysqli_error($conn);
         }
     
     }
-}?>
+?>
